76fb4c9fbee91da8fd7d3a9a5b45d0f436ec3a9d
kimyu0218 <serena35@ewhain.net>
2023-11-06 00:08:27 +0900

    Feat: app.module 수정
    - MembersModule, FeaturesModule 추가
    - ConfigModule을 이용하여 .env 파일의 환경변수 ConfigService에 등록
    - isGlobal을 true로 설정하여 루트 모듈에서 로드하면 다른 모듈에서
      ConfigModule을 가져올 필요가 없도록 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeatureModule } from './feature/feature.module';
+import { MembersModule } from './members/members.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MembersModule,
+    FeatureModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
