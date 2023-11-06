d90dbed695a0e4b76acf74358d325e0641e0f403
kimyu0218 <serena35@ewhain.net>
2023-11-06 22:55:56 +0900

    Feat: main 수정
    - validation 파이프 전역 범위의 파이프로 등록

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from './common/pipe/validation.pipe';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication<any> = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
