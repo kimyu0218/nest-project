66023bf13599af3764961b83f29e9d70d7d4d644
kimyu0218 <serena35@ewhain.net>
2023-11-07 18:30:49 +0900

    Feat: app 컨트롤러 수정
    - GET / : @Render([template_name])를 이용하여 템플릿 렌더링
    - GET /dynamic : @Res()를 이용하여 템플릿 동적으로 렌더링

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,15 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return { message: 'Hello, nest!' };
+  }
+
+  @Get('/dynamic')
+  dynamicRender(@Res() res: Response) {
+    return res.render(this.appService.getViewName(), {
+      message: 'Dynamic Rendering',
+    });
   }
 }
